08ec76dd0be4ac4c791cedd42e2bab8db5519f55
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2024-12-03 20:41:19 -0300

    Testando acessibilidade da página

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { useEffect } from "react";
+
+import "@/styles/globals.css";
+
+import { axeAccessibilityReporter } from '@/utils/axeAccessibilityReporter'
+
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    axeAccessibilityReporter()
+  }, [])
+
   return <Component {...pageProps} />;
 }
